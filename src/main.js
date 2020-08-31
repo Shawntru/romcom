8785c6f6b0699198c103a7daea6cd1563dc65eef
Kara Caputo <kncaputo@gmail.com>
2020-08-31 18:53:09 -0400

    Refactor createHTML function and alphabetize querySelectors

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 // Create variables targetting the relevant DOM elements here 👇
-var formView = document.querySelector('.form-view');
-var homeView = document.querySelector('.home-view');
-var randomCov = document.querySelector('.random-cover-button');
-var saveCov = document.querySelector('.save-cover-button');
-var homeButton = document.querySelector('.home-button');
-var savedView = document.querySelector('.saved-view');
-var makeNew = document.querySelector('.make-new-button');
 var coverImg = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
-var viewSaved = document.querySelector('.view-saved-button');
 var createNew = document.querySelector('.create-new-book-button');
-var tag1 = document.querySelector('.tagline-1');
-var tag2 = document.querySelector('.tagline-2');
+var formView = document.querySelector('.form-view');
+var homeButton = document.querySelector('.home-button');
+var homeView = document.querySelector('.home-view');
+var makeNew = document.querySelector('.make-new-button');
+var randomCov = document.querySelector('.random-cover-button');
+var saveCov = document.querySelector('.save-cover-button');
 var savedCoverSection = document.querySelector('.saved-covers-section');
-
+var savedView = document.querySelector('.saved-view');
+var viewSaved = document.querySelector('.view-saved-button');
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -21,13 +18,12 @@ var savedCovers = [
 var currentCover = {};
 
 // Add your event listeners here 👇
-randomCov.addEventListener('click', getRandomCover);
-makeNew.addEventListener('click', showForm);
-viewSaved.addEventListener('click', showSaved);
-homeButton.addEventListener('click', showHome);
 createNew.addEventListener('click', makeUserBook);
+homeButton.addEventListener('click', showHome);
+makeNew.addEventListener('click', showForm);
+randomCov.addEventListener('click', getRandomCover);
 saveCov.addEventListener('click', saveBook);
-// savedCoverSection.addEventListener('dblclick', deleteSavedBook());
+viewSaved.addEventListener('click', showSaved);
 
 // Create your event handlers and other functions here 👇
 getRandomCover();
@@ -38,8 +34,8 @@ function getRandomIndex(array) {
 
 function displayCover(currentCover) {
   coverTitle.innerText = currentCover.title;
-  tag1.innerText = currentCover.tagline1;
-  tag2.innerText = currentCover.tagline2;
+  document.querySelector('.tagline-1').innerText = currentCover.tagline1;
+  document.querySelector('.tagline-2').innerText = currentCover.tagline2;
   coverImg.setAttribute('src', currentCover.cover);
 }
 
@@ -77,74 +73,44 @@ function makeUserBook() {
 function saveBook() {
   if (!savedCovers.includes(currentCover)) {
     savedCovers.push(currentCover);
-    savedCoverSection.insertAdjacentHTML('afterbegin', `
-      <div class="mini-cover" id="${currentCover.id}" ondblclick="deleteSavedBook(${currentCover.id})">
-      <p id="explosion" ondblclick="animateScript()" onmouseout="stopAnimate()">
-      <img class="mini-cover" src="${currentCover.cover}">
-      <h2 class="cover-title">${currentCover.title}</h2>
-      <h3 class="tagline">A tale of ${currentCover.tagline1} and ${currentCover.tagline2}<h3>
-      <img class="price-tag" src="./assets/price.png">
-      <img class="overlay" src="./assets/overlay.png">
-      <p/>
-      </div>
-    `);
+    createHTML();
   }
+}
+
+function createHTML() {
+  var htmlBlock = `<div class="mini-cover" id="${currentCover.id}" ondblclick="deleteSavedBook(${currentCover.id})">
+  <img class="mini-cover" src="${currentCover.cover}">
+  <h2 class="cover-title">${currentCover.title}</h2>
+  <h3 class="tagline">A tale of ${currentCover.tagline1} and ${currentCover.tagline2}<h3>
+  <img class="price-tag" src="./assets/price.png">
+  <img class="overlay" src="./assets/overlay.png">
+  </div>`;
+  savedCoverSection.insertAdjacentHTML('afterbegin', htmlBlock);
 }
 
 function showForm() {
   formView.classList.remove('hidden');
+  homeButton.classList.remove('hidden');
   homeView.classList.add('hidden');
   randomCov.classList.add('hidden');
   saveCov.classList.add('hidden');
-  homeButton.classList.remove('hidden');
   savedView.classList.add('hidden');
 }
 
 function showSaved() {
-  savedView.classList.remove('hidden');
   formView.classList.add('hidden');
+  homeButton.classList.remove('hidden');
   homeView.classList.add('hidden');
   randomCov.classList.add('hidden');
   saveCov.classList.add('hidden');
-  homeButton.classList.remove('hidden');
+  savedView.classList.remove('hidden');
 }
 
 function showHome() {
-  savedView.classList.add('hidden');
   formView.classList.add('hidden');
+  homeButton.classList.add('hidden');
   homeView.classList.remove('hidden');
   randomCov.classList.remove('hidden');
   saveCov.classList.remove('hidden');
-  homeButton.classList.add('hidden');
+  savedView.classList.add('hidden');
 }
-
-var tID; //we will use this variable to clear the setInterval()
-
-function stopAnimate() {
-  clearInterval(tID);
-} //end of stopAnimate()
-
-
-function animateScript() {
-
-  var position = 128; //start position for the image slicer
-  const interval = 100; //100 ms of interval for the setInterval()
-  const diff = 128; //diff as a variable for position offset
-
-  tID = setInterval(() => {
-
-    document.getElementById("explosion").styles.backgroundPosition =
-      `-${position}px 0px`;
-    //we use the ES6 template literal to insert the variable "position"
-
-    if (position < 1920) {
-      position = position + diff;
-    }
-    //we increment the position by 256 each time
-    else {
-      position = 128;
-    }
-    //reset the position to 256px, once position exceeds 1536px
-
-  }, interval); //end of setInterval
-} //end of animateScript()
